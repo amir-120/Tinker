8fe9c559b8ca8cfc3ab8e2d67ef7aa5095ae1e51
Darkness <amir008hosein@gmail.com>
2022-08-30 01:21:20 +0430

    1

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <wiringPi.h>
 
-int main(){
-    printf("Test\n");
-    getchar( );
-    return 0;
+// LED Pin - wiringPi pin 0 is ASSU_PI_GPIO 164.
+
+#define	LED	0
+
+int main (void) {
+  printf("Raspberry Pi blink\n");
+
+  wiringPiSetup();
+  pinMode(LED, OUTPUT);
+
+  while(true) {
+    digitalWrite(LED, HIGH);	// On
+    delay(500);		// mS
+    digitalWrite(LED, LOW);	// Off
+    delay(500);
+  }
+  return 0;
 }
